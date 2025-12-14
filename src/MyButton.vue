07ba3aa3e9fa56443967ca7ce987205abca105e6
liyansheng <1761724207@qq.com>
2025-12-15 00:23:18 +0800

    feat: 初始验证组件

---
+++ src/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="my-btn">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MyButton'
+}
+</script>
+
+<style scoped>
+.my-btn {
+  padding: 8px 16px;
+  background: #409eff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+}
+</style>
